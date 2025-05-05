d984a8c2943c06bbecfb13e56232b7955a07c437
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2025-05-05 15:53:58 -0400

    testing recursive kv

--- example.py
+++ example.py
@@ -1,21 +1,20 @@
-import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
 from src.hf_cache import KNormCache
-from src.kvmerger import KVMerger
+from src.value_aware import AverageCache
+from src.recursive import RecursiveCache
 from accelerate import disk_offload
 from datasets import load_dataset
-import bitsandbytes # NOTE: weird grad error due to no GPU
+import torch
 
-model_name = "meta-llama/Llama-2-3b-chat-hf"
+model_name = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
+    model_name, 
     device_map="auto",
     low_cpu_mem_usage=True,
-    #MPS doesn't support bfloat, for some reason float16 also does not work
+    torch_dtype="bfloat16",
     attn_implementation="sdpa"
 )
 
-model = torch.compile(model)
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 streamer = TextStreamer(tokenizer)
@@ -25,16 +24,18 @@ input_text = f"<|User|>{question}<|Assistant|><think>\n"
 
 inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
 
-past_key_values = KNormCache(
-    window_length=4,
-    max_length=8,
-)
-
-#past_key_values = KVMerger(
+#past_key_values = KNormCache(
 #    window_length=64,
 #    max_length=128,
-#    eps=0.9
 #)
+
+past_key_values = RecursiveCache(
+    window_length=64,
+    max_length=128,
+    eps=0.9
+)
+
+
 
 out = model.generate(
     **inputs,
